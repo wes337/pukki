bbea21c5eff85d50008f94455f055f48125335a0
wes337 <wesmoses@gmail.com>
2022-10-27 13:12:18 +0300

    page title and spacings

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from "next/head";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { Header, Card } from "../components";
@@ -8,18 +9,23 @@ export default function MyApp({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <div className="main">
-      <main>
-        <SessionContextProvider
-          supabaseClient={supabaseClient}
-          initialSession={pageProps.initialSession}
-        >
-          <Header />
-          <Card>
-            <Component {...pageProps} />
-          </Card>
-        </SessionContextProvider>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Pukki</title>
+      </Head>
+      <div className="main">
+        <main>
+          <SessionContextProvider
+            supabaseClient={supabaseClient}
+            initialSession={pageProps.initialSession}
+          >
+            <Header />
+            <Card>
+              <Component {...pageProps} />
+            </Card>
+          </SessionContextProvider>
+        </main>
+      </div>
+    </>
   );
 }
