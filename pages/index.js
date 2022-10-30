63d88929d5f43a02f59bfe0cf3e5506d464886e3
wes337 <wesmoses@gmail.com>
2022-10-30 13:26:15 +0200

    show loader

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,30 @@ import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { updateUser } from "../actions/users";
 import Users from "./users";
+import { Loader } from "../components";
 
 export default function Index() {
+  const [loading, setLoading] = useState(false);
   const [ready, setReady] = useState(false);
   const supabaseClient = useSupabaseClient();
   const session = useSession();
 
   useEffect(() => {
+    setLoading(true);
     if (!session) {
+      setLoading(false);
       setReady(false);
     } else {
-      updateUser(session.user).then(() => setReady(true));
+      updateUser(session.user).then(() => {
+        setReady(true);
+        setLoading(false);
+      });
     }
   }, [session]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return ready ? (
     <Users />
