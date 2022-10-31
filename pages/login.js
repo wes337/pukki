59a663785b058425d9c360a17a1aed8b509cca13
wes337 <wesmoses@gmail.com>
2022-10-31 18:05:17 +0200

    push to users after login

--- pages/login.js
+++ pages/login.js
@@ -3,14 +3,14 @@ import { useRouter } from "next/router";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 
-export default function Index() {
+export default function Login() {
   const router = useRouter();
   const user = useUser();
   const supabaseClient = useSupabaseClient();
 
   useEffect(() => {
     if (user) {
-      router.push("/");
+      router.push("/users");
     }
   }, [user]);
 
