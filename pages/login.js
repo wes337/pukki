25adbb64af8b234910147293201962f1144c0a09
wes337 <wesmoses@gmail.com>
2022-11-15 20:49:45 +0200

    update packages

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import {
+  useSession,
+  useSupabaseClient,
+  useUser,
+} from "@supabase/auth-helpers-react";
 import useTranslate from "../hooks/useTranslate";
 
 export default function Login() {
   const router = useRouter();
   const user = useUser();
+  const session = useSession();
   const supabaseClient = useSupabaseClient();
   const translate = useTranslate();
 
   useEffect(() => {
-    if (user) {
+    if (user || session) {
       router.push("/users");
     }
-  }, [user, router]);
+  }, [user, session, router]);
 
   return (
     <Auth
