10127c3e893a8e632c83f29d894f8a121e7d5e8a
wes337 <wesmoses@gmail.com>
2022-11-15 20:30:45 +0200

    clear all cookies and storage when logging out

--- components/SignOut/SignOut.js
+++ components/SignOut/SignOut.js
@@ -12,6 +12,32 @@ export default function SignOut() {
   const router = useRouter();
   const translate = useTranslate();
 
+  const signOut = () => {
+    sessionStorage.clear();
+
+    localStorage.clear();
+
+    caches.keys().then((keys) => {
+      keys.forEach((key) => caches.delete(key));
+    });
+
+    indexedDB.databases().then((dbs) => {
+      dbs.forEach((db) => indexedDB.deleteDatabase(db.name));
+    });
+
+    document.cookie = document.cookie.split(";").reduce((cookie, keyVal) => {
+      var pair = keyVal.trim().split("=");
+      if (pair[0]) {
+        if (pair[0] !== "path" && pair[0] !== "expires") {
+          cookie += pair[0] + "=;";
+        }
+      }
+      return cookie;
+    }, "expires=Thu, 01 Jan 1970 00:00:00 UTC; path:/;");
+
+    supabase.auth.signOut().then(() => router.push("/"));
+  };
+
   return (
     <header className={styles["sign-out"]}>
       <Logo centered={!user} />
@@ -21,12 +47,7 @@ export default function SignOut() {
             name: <span>{getFirstName(getUserName(user))}</span>,
           })}
           <hr />
-          <Button
-            icon="tag"
-            variant="link"
-            size="small"
-            onClick={() => supabase.auth.signOut().then(() => router.push("/"))}
-          >
+          <Button icon="tag" variant="link" size="small" onClick={signOut}>
             {translate("sign-out")}
           </Button>
         </div>
