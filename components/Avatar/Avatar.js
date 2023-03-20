98906b44ddcca4e67cd7e5b697607d32336e875c
wes337 <wesmoses@gmail.com>
2023-03-20 10:28:37 +0200

    use avatar placeholder

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./Avatar.module.scss";
 
 export default function Avatar({ url, size = 30 }) {
+  const [src, setSrc] = useState(url);
+
   return (
     <div className={styles.avatar}>
-      <Image src={url} height={size} width={size} alt="" />
+      <Image
+        src={src}
+        height={size}
+        width={size}
+        alt=""
+        onError={() => setSrc("/images/avatar-placeholder-1.png")}
+      />
     </div>
   );
 }
